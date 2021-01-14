0c62ea3934be663bdb76c341c11c53018bfcd8e2
brewchetta <chett.tiller@gmail.com>
2021-01-14 10:02:46 -0500

    added category selector element for ease of building

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ const URL = `http://localhost:${PORT}/elements`
 
 const atomicElementDetails = document.querySelector('#atomic-element-details')
 const atomicElementsContainer = document.querySelector('#atomic-elements-container')
+const categorySelector = document.querySelector('#category-select')
 
 // Atomic Element Class //
 
