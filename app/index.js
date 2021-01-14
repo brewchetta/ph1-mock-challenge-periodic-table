69db6dc391353105005544e1bb6655c26ed7217a
brewchetta <chett.tiller@gmail.com>
2021-01-14 09:33:45 -0500

    Add elements to container

--- app/index.js
+++ app/index.js
@@ -20,8 +20,32 @@ class AtomicElement {
     .then(data => data.forEach(el => new AtomicElement(el)))
   }
 
-  constructor(elementObject) {
-    console.log(elementObject)
+  constructor(obj) {
+    // There's a way to do mass assignment however I've chosen to assign each property individually for clarity
+    this.id = obj.id
+    this.name = obj.name
+    this.symbol = obj.symbol
+    this.atomicWeight = obj.atomicWeight
+    this.electronsPerShell = obj.electronsPerShell
+    this.category = obj.category
+    // next we build the HTML element
+    this.createHTMLElement()
+  }
+
+  createHTMLElement() {
+    this.htmlElement = document.createElement('div')
+    this.htmlElement.className = "atomic-element-card"
+    const pID = document.createElement('p')
+    pID.innerText = this.atomicNumber
+    const pSymbol = document.createElement('p')
+    pSymbol.innerText = this.symbol
+    this.htmlElement.append(pID, pSymbol)
+    atomicElementsContainer.append(this.htmlElement)
+  }
+
+  // Adding alternate getter for atomic number
+  get atomicNumber() {
+    return this.id
   }
 
 }
